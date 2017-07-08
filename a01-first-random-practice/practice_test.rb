87bf6f08efd8bdc282ce4a24ceef5e2d7c432a25
Matthias Jenny <matthiascjenny@gmail.com>
2017-07-08 11:50:12 -0400

    Create first random practice assessment

---
+++ a01-first-random-practice/practice_test.rb
@@ -0,0 +1,56 @@
+#returns all subsets of an array
+def subsets(array)
+
+end
+
+class Array
+
+  def my_rotate(num)
+
+  end
+
+end
+
+class Array
+
+  # Takes a multi-dimentional array and returns a single array of all the elements
+  # [1,[2,3], [4,[5]]].my_controlled_flatten(1) => [1,2,3,4,5]
+  def my_flatten
+
+  end
+
+  # Write a version of flatten that only flattens n levels of an array.
+  # E.g. If you have an array with 3 levels of nested arrays, and run
+  # my_flatten(1), you should return an array with 2 levels of nested
+  # arrays
+  #
+  # [1,[2,3], [4,[5]]].my_controlled_flatten(1) => [1,2,3,4,[5]]
+  def my_controlled_flatten(n)
+
+  end
+end
+
+class Hash
+
+  # Write a version of merge. This should NOT modify the original hash
+  def my_merge(hash2)
+
+  end
+
+end
+
+# Write a method that returns the factors of a number in ascending order.
+
+def factors(num)
+
+end
+
+class Array
+
+  #Write a monkey patch of quick sort that accepts a block
+  def my_quick_sort(&prc)
+
+  end
+  
+end
+
---
+++ aa-prep/a01-test-generator-mallory/spec.rb
@@ -0,0 +1,84 @@
+require 'rspec'
+require_relative 'practice_test'
+describe 'subsets' do
+
+  it "Correctly returns all subsets of an array" do
+    expect(subsets([1, 2, 3])).to eq([[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]])
+  end
+
+end
+
+describe "my_rotate" do
+  a = [ "a", "b", "c", "d" ]
+
+  it "Rotates the elements 1 position if no argument is passed in" do
+    expect(a.my_rotate).to eq(["b", "c", "d", "a"])
+  end
+
+  it "Rotates the elements correctly if an argument is passed in" do
+    expect(a.my_rotate(2)).to eq(["c", "d", "a", "b"])
+  end
+
+  it "Rotates the elements correctly if a negative argument is passed in" do
+    expect(a.my_rotate(-3)).to eq(["b", "c", "d", "a"])
+  end
+
+  it "Rotates the elements correctly for a large argument" do
+    expect(a.my_rotate(15)).to eq(["d", "a", "b", "c"])
+  end
+
+end
+
+describe "my_flatten" do
+  it 'Flattens arrays correctly' do
+    expect([1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten).to eq([1, 2, 3, 4, 5, 6, 7, 8])
+  end
+end
+
+describe "my_controlled_flatten" do
+  it "Flattens an array the specified number of levels" do
+    expect([1,[2,3], [4,[5]], [[6,[7]]]].my_controlled_flatten(1)).to eq([1,2,3,4,[5], [6, [7]]])
+  end
+end
+
+describe "my_merge" do
+  a = {a: 1, b: 2, c: 3}
+  b = {d: 4, e: 5}
+  c = {c: 33, d: 4, e: 5}
+
+  it "Merges 2 hashes and returns a hash" do
+      expect(a.my_merge(b)).to eq(a.merge(b))
+  end
+
+  it "Priortizes values from the hash being merged in" do
+      expect(a.my_merge(c)).to eq(a.merge(c))
+  end
+end
+
+describe "#factors" do
+  it "returns the factors of 10 in order" do
+    expect(factors(10)).to eq([1, 2, 5, 10])
+  end
+
+  it "returns just two factors for primes" do
+    expect(factors(13)).to eq([1, 13])
+  end
+end
+
+describe "my_quick_sort" do
+
+  a = (0..8).to_a
+
+  it "Sorts an array of numbers with no duplicates" do
+    expect(a.shuffle.my_quick_sort).to eq(a)
+  end
+
+  it "Sorts an array of numbers with duplicates" do
+    expect([1,2,3,3,9,10,10,17,432].shuffle.my_quick_sort).to eq([1,2,3,3,9,10,10,17,432])
+  end
+
+  it "Sorts according to the block passed in" do
+    expect(a.shuffle.my_quick_sort{|a,b| b<=>a}).to eq(a.reverse)
+  end
+end
+
