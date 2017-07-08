3acb142e91b65ec8cf876697fb654158caa1fc14
Matthias Jenny <matthiascjenny@gmail.com>
2017-07-08 14:21:11 -0400

    Finish practice third retry

---
+++ a01-third-random-practice/practice_test.rb
@@ -0,0 +1,61 @@
+# return b^n recursively. Your solution should accept negative values
+# for n
+def exponent(b, n)
+
+end
+
+class Array
+
+  # Write an Array#dups method that will return a hash containing the indices of all
+  # duplicate elements. The keys are the duplicate elements; the values are
+  # arrays of their indices in ascending order, e.g.
+  # [1, 3, 4, 3, 0, 3, 0].dups => { 3 => [1, 3, 5], 0 => [4, 6] }
+
+  def dups
+  end
+end
+
+class Array
+
+  # Takes a multi-dimentional array and returns a single array of all the elements
+  # [1,[2,3], [4,[5]]].my_controlled_flatten(1) => [1,2,3,4,5]
+  def my_flatten
+
+  end
+
+  # Write a version of flatten that only flattens n levels of an array.
+  # E.g. If you have an array with 3 levels of nested arrays, and run
+  # my_flatten(1), you should return an array with 2 levels of nested
+  # arrays
+  #
+  # [1,[2,3], [4,[5]]].my_controlled_flatten(1) => [1,2,3,4,[5]]
+  def my_controlled_flatten(n)
+
+  end
+end
+
+# Write a method that returns the factors of a number in ascending order.
+
+def factors(num)
+
+end
+
+class Array
+
+  # Write a monkey patch of binary search:
+  # E.g. [1, 2, 3, 4, 5, 7].my_bsearch(5) => 4
+  def my_bsearch(target, &prc)
+
+  end
+
+end
+
+class Array
+
+  #Write a monkey patch of quick sort that accepts a block
+  def my_quick_sort(&prc)
+
+  end
+  
+end
+
