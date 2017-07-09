8ab6f72dd2bf3cdb41d9941059a8bf91b474036f
Matthias Jenny <matthiascjenny@gmail.com>
2017-07-09 10:09:32 -0400

    Practice bsearch again

--- bsearch/bsearch.rb
+++ bsearch/bsearch.rb
@@ -1,23 +1,103 @@
-require 'byebug'
-
 class Array
 
   def bsearch(target)
-    debugger
     return nil if self.length == 0
 
     mid = self.length / 2
 
     case self[mid] <=> target
     when 0
-      return mid
+      mid
     when 1
       self.take(mid).bsearch(target)
     else
       result = self.drop(mid + 1).bsearch(target)
       result.nil? ? nil : mid + 1 + result
     end
-
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'byebug'
+#
+# class Array
+#
+#   def bsearch(target)
+#     debugger
+#     return nil if self.length == 0
+#
+#     mid = self.length / 2
+#
+#     case self[mid] <=> target
+#     when 0
+#       return mid
+#     when 1
+#       self.take(mid).bsearch(target)
+#     else
+#       result = self.drop(mid + 1).bsearch(target)
+#       result.nil? ? nil : mid + 1 + result
+#     end
+#
+#   end
+#
+# end
