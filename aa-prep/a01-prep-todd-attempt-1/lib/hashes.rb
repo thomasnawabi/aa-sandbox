606ebe21be8b6bae55e11140fded26e636c58eec
Matthias Jenny <matthiascjenny@gmail.com>
2017-07-09 13:46:19 -0400

    Finish hashes in first attempt at Todd's problems

--- aa-prep/a01-prep-todd-attempt-1/lib/hashes.rb
+++ aa-prep/a01-prep-todd-attempt-1/lib/hashes.rb
@@ -3,9 +3,22 @@
 class Hash
 
   def my_each(&prc)
+    result = Hash.new
+    self.keys.each do |k|
+      result[k] = prc.call(k, self[k])
+    end
+    result
   end
 
   def my_merge(hash2)
+    result = Hash.new
+    self.my_each do |k, v|
+      result[k] = v
+    end
+    hash2.my_each do |k, v|
+      result[k] = v
+    end
+    result
   end
 
 end
